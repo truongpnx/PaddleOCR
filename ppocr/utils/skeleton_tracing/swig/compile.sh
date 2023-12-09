13cae093b40b4676a7e1c71516f82bead59221d3
pnxuantruong <xuantruonpnxt@gmail.com>
2023-12-09 14:06:15 +0700

    update skeleton

--- ppocr/utils/skeleton_tracing/swig/compile.sh
+++ ppocr/utils/skeleton_tracing/swig/compile.sh
@@ -1,9 +1,8 @@
-
 swig -python trace_skeleton.i
-gcc -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I/usr/local/Cellar/python/3.7.6_1/Frameworks/Python.framework/Versions/3.7/include/python3.7m
-gcc $(python3-config --ldflags) -dynamiclib *.o -o _trace_skeleton.so -I/usr/local/Cellar/python/3.7.6_1/Frameworks/Python.framework/Versions/3.7/lib/libpython3.7m.dylib -undefined dynamic_lookup
+gcc -fPIC -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I/$(python3-config --cflags)
+g++ -shared $(python3-config --cflags --ldflags) *.o -o _trace_skeleton.so
 
 # quick tests
 # python3 -i -c "import trace_skeleton; trace_skeleton.trace('\0\0\0\1\1\1\0\0\0',3,3); print(trace_skeleton.len_polyline());"
 # python3 -i -c "import trace_skeleton; print(trace_skeleton.from_list([0,0,0,1,1,1,0,0,0],3,3))"
-python3 example.py
+# python3 example.py
